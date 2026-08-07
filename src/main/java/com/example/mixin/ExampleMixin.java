370a9b96f3dc9c0c2823176dc3a3810bd6f401fa
kartiksoni9302623196-creator <kartiksoni9302623196@gmail.com>
2026-08-07 14:28:48 +0530

    Change mixin target from TitleScreen to MinecraftServer

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,9 @@
 package com.example.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(MinecraftServer.class)
 public class ExampleMixin {
-    @Inject(at = @At("HEAD"), method = "init()V")
-    private void init(CallbackInfo info) {
-        // Boilerplate mixin
-    }
 }
+
